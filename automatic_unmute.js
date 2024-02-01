f445f21d9996800643b2de0da6165264f5cca8d4
Hanwy <mr.hanwy@outlook.com>
2024-02-02 00:34:37 +0800

    更改 description

--- automatic_unmute.js
+++ automatic_unmute.js
@@ -2,7 +2,7 @@
 // @name         自动取消静音
 // @namespace    https://github.com/mr-hanwy/Scripts
 // @version      2024-02-01
-// @description  自动取消静音
+// @description  现代卓越自动播放时，自动取消播放器静音
 // @author       Hanwy
 // @match        https://remote.chinapm.org/course/*/task/*/show
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
