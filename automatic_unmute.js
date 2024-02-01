0045493ab8296214f9f66b4762a681435fb090bc
Hanwy <mr.hanwy@outlook.com>
2024-02-02 00:21:38 +0800

    初始化文件

---
+++ automatic_unmute.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         自动取消静音
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-01
+// @description  自动取消静音
+// @author       Hanwy
+// @match        https://remote.chinapm.org/course/*/task/*/show
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
